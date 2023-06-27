868b3b4dccba8ffd1ac1c0ac24fec1cfbd9dbf90
Vladislav <bllxy67@gmail.com>
2023-06-27 15:53:08 +0300

    initialized config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
+  ],
 })
 export class AppModule {}
