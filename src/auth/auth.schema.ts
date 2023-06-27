799968897042f32295ee8003ee72c3b4621bf8b3
Vladislav <bllxy67@gmail.com>
2023-06-27 17:21:32 +0300

    feat: add user schema

--- src/auth/auth.schema.ts
+++ src/auth/auth.schema.ts
@@ -1,7 +1,9 @@
-import { Prop, Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Exclude, Type } from 'class-transformer';
 import mongoose from 'mongoose';
 import { User } from 'src/user/user.schema';
+
+export type AuthDocument = Auth & Document;
 
 @Schema({
   toJSON: {
@@ -17,3 +19,5 @@ export class Auth {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true })
   user: User;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(Auth);
