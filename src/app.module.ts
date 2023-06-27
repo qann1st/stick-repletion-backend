06666bc47318f8934f5f48fb3285ee1aaadce452
Vladislav <bllxy67@gmail.com>
2023-06-27 19:06:03 +0300

    feat: add AuthModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
