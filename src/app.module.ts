ad22bfbcc82e8b0033defe2cd57d27dfe41a645b
Vladislav <bllxy67@gmail.com>
2023-06-27 17:13:16 +0300

    feat: add user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    UserModule,
   ],
 })
 export class AppModule {}
