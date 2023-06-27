3d00c5772f6c392a025f2690f22fcb12bda5e8af
Vladislav <bllxy67@gmail.com>
2023-06-27 17:03:31 +0300

    fix: mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URL),
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
 })
 export class AppModule {}
