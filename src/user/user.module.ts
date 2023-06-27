37c1fd677fc4cc62eb6942b2cc3d1f7f007a2340
Vladislav <bllxy67@gmail.com>
2023-06-27 16:44:24 +0300

    feat: add userController in controllers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ schema: UserSchema, name: User.name }]),
   ],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
