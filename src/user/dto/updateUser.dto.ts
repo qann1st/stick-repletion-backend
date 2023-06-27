3eb57f7e2c6ee4e1d746856d224ffaf1994d4ddb
Vladislav <bllxy67@gmail.com>
2023-06-27 16:33:42 +0300

    feat: add avatar on updateUser

--- src/user/dto/updateUser.dto.ts
+++ src/user/dto/updateUser.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './createUser.dto';
+import { IsUrl } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsUrl()
+  avatar: string;
+}
