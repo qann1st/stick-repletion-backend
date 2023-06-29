beaa6c3904913480dce05e5f2a75d62144e823fa
Vladislav <bllxy67@gmail.com>
2023-06-29 17:01:04 +0300

    feat: create question schema

---
+++ src/questions/questions.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Exclude } from 'class-transformer';
+import mongoose, { Document } from 'mongoose';
+import { User } from 'src/user/user.schema';
+
+export type QuestionDocument = Question & Document;
+
+@Schema({
+  toJSON: {
+    virtuals: true,
+  },
+})
+export class Question {
+  @Exclude()
+  _id: string;
+  @Exclude()
+  __v: number;
+  @Prop({
+    type: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } }],
+  })
+  owner: User;
+  @Prop({ required: true, minlength: 2, maxlength: 32 })
+  title: string;
+  @Prop({ required: true, minlength: 20 })
+  problem: string;
+  @Prop({ required: true, minlength: 20 })
+  attemptsFix: string;
+  @Prop({ required: true })
+  tags: string[];
+}
+
+export const QuestionSchema = SchemaFactory.createForClass(Question);
