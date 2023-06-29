1d138e099038a4d78e630b33538751193037e4ea
Vladislav <bllxy67@gmail.com>
2023-06-29 19:42:47 +0300

    feat: edit owner type

--- src/questions/questions.schema.ts
+++ src/questions/questions.schema.ts
@@ -1,9 +1,14 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Exclude } from 'class-transformer';
 import mongoose, { Document } from 'mongoose';
 import { User } from 'src/user/user.schema';
 
 export type QuestionDocument = Question & Document;
+type QuestionUser = Pick<
+  User,
+  '_id' | 'avatar' | 'createdAccount' | 'email' | 'questions' | 'username'
+>;
 
 @Schema({
   toJSON: {
@@ -16,9 +21,9 @@ export class Question {
   @Exclude()
   __v: number;
   @Prop({
-    type: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } }],
+    type: PartialType(User),
   })
-  owner: User;
+  owner: QuestionUser;
   @Prop({ required: true, minlength: 2, maxlength: 32 })
   title: string;
   @Prop({ required: true, minlength: 20 })
